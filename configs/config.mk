f3657f4a702623dad4dab83505b5b24c968b0818
ShoreDroid <53381903+PACROM@users.noreply.github.com>
2019-08-02 22:04:52 -0400

    bringing in some grimlock

---
+++ configs/config.mk
@@ -0,0 +1,12 @@
+#
+# PACROM Grimlock Apps Inclusion Makefile
+#
+
+GRIM_PATH := vendor/pac/prebuilt_apps
+
+# app    
+PRODUCT_PACKAGES += \
+    OTAprebuilt.apk \
+    changeprebuilt.apk \
+    forumprebuilt.apk
+
